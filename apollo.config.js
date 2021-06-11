c39790a3cad16e1c8f14676b5fd1caab747132c2
IbrahimShahzad <ibrahim.shahzad@techavenue.biz>
2021-06-12 01:12:28 +0500

    cfg

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   client: {
-    pathPrefix: '/ibrahim',
     addTypename: false,
     excludes: [],
     includes: ["./src/**/*.tsx","./src/**/*.ts","./plugins/**/*.js","./node_modules/gatsby-source-contentful/src/fragments.js","./node_modules/gatsby-source-datocms/fragments/*.js","./node_modules/gatsby-source-sanity/fragments/*.js","./node_modules/gatsby-transformer-sharp/src/fragments.js"],
